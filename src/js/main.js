1f9c1bf132b86d2be21b46b8dc31c350478ee0af
BegovicNikola <begovic.office@gmail.com>
2019-03-05 02:09:46 +0100

    Initial Commit

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+    const navholder = document.getElementById('navholder');
+    // Calling func
+    getNav();
+    // Getting nav func
+    function getNav(){
+        $.ajax({
+            url: "src/assets/data/navigation.json",
+            method: "GET",
+            dataType: "json",
+            success: data => {
+                renderNav(data);
+            },
+            error: err => {
+                console.log(err);
+            }
+        })
+    }
+    // Render nav func
+    function renderNav(navigation){
+        let currentPage = window.location.href;
+        function activeNav(data, param){
+            if(data.path == param){
+                return data.imga;
+            }else{
+                return data.img;
+            }
+        }
+        let html = `
+            <button class="navbar-toggler w-100" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav w-100 d-flex justify-content-center">
+        `;
+        navigation.forEach(nav => {
+            html += `
+                <li class="nav-item d-flex justify-content-center">
+                    <a class="nav-link d-flex flex-column align-items-center" href="${nav.path}">
+                        <img style="width: 100px;" src="${activeNav(nav, currentPage)}" alt="${nav.name}" />
+                        ${nav.name}
+                    </a>
+                </li>
+            `;
+        })
+        html += `
+                </ul>
+            </div>
+        `;
+        navholder.innerHTML = html;
+    }
+}
